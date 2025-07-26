65b4865db027db277eda4711d72657b0ca2b3153
murat atak <muratatak@murats-MacBook-Pro-2.local>
2025-07-26 11:34:59 -0600

    fix: use correct dist path for Angular Universal output

--- frontend/server.js
+++ frontend/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/frontend/browser'));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/frontend/browser/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
