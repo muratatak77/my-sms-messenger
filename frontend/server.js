38a2a74dd2b62ef6da94881f561d92e11050e26f
murat atak <muratatak@murats-MacBook-Pro-2.local>
2025-07-26 12:01:09 -0600

    Fixed the server,js

--- frontend/server.js
+++ frontend/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/frontend/browser'));
+// Güncellenmiş yol
+app.use(express.static(path.join(__dirname, 'dist/frontend')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/frontend/browser/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/frontend/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server listening on port ${process.env.PORT || 8080}`);
+});
